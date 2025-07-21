0276e9a15700a80ffbb3246bdede3c108707af94
mjekrami <82811963+mjekrami@users.noreply.github.com>
2025-07-21 09:21:55 +0000

    modified footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="py-8 px-4 border-t border-gray-200 text-center text-gray-500 bg-white">
       <div className="max-w-6xl mx-auto">
-        <p className="font-light">© {new Date().getFullYear()} Alex Chen. All rights reserved.</p>
+        <p className="font-light">© {new Date().getFullYear()} Mohammad Ekrami. All rights reserved.</p>
       </div>
     </footer>
   )
